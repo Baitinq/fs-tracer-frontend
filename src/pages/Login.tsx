74612f574d6057325a7b90e7d002f441f0fd6f13
Baitinq <manuelpalenzuelamerino@gmail.com>
2024-06-25 23:52:30 +0200

    Login: Improve login screen aesthetics pt2

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -27,28 +27,43 @@ export default function Login(props: any) {
     }
   }, [props.session])
 
+  //min-w-full broke the container
   return (
     <>
-      <div className="flex flex-col items-center">
-        <h1 className="text-blue-700">Login</h1>
+      <div className="flex flex-col items-center h-screen w-screen">
+        <div className="flex flex-row items-center gap-x-8 m-auto">
+          <div>
+            <h1 className="text-4xl text-bold font-sans antialiased text-blue-700 tracking-wide">fs-tracer</h1>
+          </div>
 
-        <GoogleOAuthProvider clientId="952965459060-nrnrsdoq22mf646vfa72hk410pvdda5q.apps.googleusercontent.com">
-          <GoogleLogin
-            onSuccess={async (credentialResponse: any) => {
-              await props.supabase.auth.signInWithIdToken({
-                provider: 'google',
-                token: credentialResponse.credential,
-              })
+          <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div className="mx-7 my-5">
+              <h5 className="mb-2 text-2xl font-bold font-sans antialiased text-gray-900">Hello!</h5>
+              <div className="flex flex-row items-center gap-2">
+                <span className="font-normal text-gray-700 dark:text-gray-400" >Continue with</span>
+                <span>
+                  <GoogleOAuthProvider clientId="952965459060-nrnrsdoq22mf646vfa72hk410pvdda5q.apps.googleusercontent.com">
+                    <GoogleLogin
+                      onSuccess={async (credentialResponse: any) => {
+                        await props.supabase.auth.signInWithIdToken({
+                          provider: 'google',
+                          token: credentialResponse.credential,
+                        })
 
-            }}
-            onError={() => {
-              console.log('Login Failed');
-            }}
-            width={7777}
-            size={"large"}
-            shape={"pill"}
-          />
-        </GoogleOAuthProvider>
+                      }}
+                      onError={() => {
+                        console.log('Login Failed');
+                      }}
+                      width={7777}
+                      size={"large"}
+                      type={"icon"}
+                    />
+                  </GoogleOAuthProvider>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
