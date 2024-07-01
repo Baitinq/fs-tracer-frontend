485678f4079aed4d5b2755c11e24b02bb32110f4
Baitinq <manuelpalenzuelamerino@gmail.com>
2024-07-01 13:25:08 +0200

    App: Save session in localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { createClient } from '@supabase/supabase-js'
 
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Logout from './pages/Logout';
+import Recent from './pages/Recent';
+import { useLocalStorage } from "@uidotdev/usehooks";
 
 const supabase = createClient('https://slpoocycjgqsuoedhkbn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNscG9vY3ljamdxc3VvZWRoa2JuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUyMDU0MjUsImV4cCI6MjAzMDc4MTQyNX0.xZYRTRN65rlms1Hb96IBAQvw3EGtMzUxlGPP5TVey34')
 
 function App() {
-  const [session, setSession] = useState(null)
+  const [session, setSession] = useLocalStorage("session", null)
   return (
     <>
       <BrowserRouter basename={import.meta.env.DEV ? '/' : '/fs-tracer-frontend'}>
@@ -18,10 +18,10 @@ function App() {
           <Route index element={
             <Home supabase={supabase} session={session} />
           } />
-          <Route path="login" element={
+          <Route path="/login" element={
             <Login supabase={supabase} session={session} setSession={setSession} />
           } />
-          <Route path="logout" element={
+          <Route path="/logout" element={
             <Logout supabase={supabase} session={session} setSession={setSession} />
           } />
           <Route path="*" element={<p>notfound</p>} />
